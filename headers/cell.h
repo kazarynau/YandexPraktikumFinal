0c0e0073c9a57d37043a06f424fc07261dff9851
Siarhei Kazarynau <skazarynau@charlesworthresearch.com>
2021-10-19 22:01:36 +0300

    Move HasCircularDependency to GraphInterface

--- headers/cell.h
+++ headers/cell.h
@@ -10,13 +10,24 @@ class Sheet;
 
 class GraphInterface {
  public:
+  // Используется Position, т.к. сказано, что нельзя менять CellInterface,
+  // поэтому нет смысла использовать указатели на ячейки
   virtual std::vector<Position> GetReferencedCells() const = 0;
   virtual std::vector<Position> GetDependentCells() const = 0;
+
+  // Если ячейка не создана, создаем EmptyImpl и в нее добавляем dependent cell
   virtual void AddDependentCell(const Position pos) = 0;
   virtual void DeleteDependentCell(const Position pos) = 0;
+
+  // https://neerc.ifmo.ru/wiki/index.php?title=%D0%98%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5_%D0%BE%D0%B1%D1%85%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%B3%D0%BB%D1%83%D0%B1%D0%B8%D0%BD%D1%83_%D0%B4%D0%BB%D1%8F_%D0%BF%D0%BE%D0%B8%D1%81%D0%BA%D0%B0_%D1%86%D0%B8%D0%BA%D0%BB%D0%B0
+  // https://neerc.ifmo.ru/wiki/index.php?title=%D0%9E%D0%B1%D1%85%D0%BE%D0%B4_%D0%B2_%D0%B3%D0%BB%D1%83%D0%B1%D0%B8%D0%BD%D1%83,_%D1%86%D0%B2%D0%B5%D1%82%D0%B0_%D0%B2%D0%B5%D1%80%D1%88%D0%B8%D0%BD
+  virtual bool HasCircularDependency(
+      std::unordered_set<CellInterface*>& visitedCells,
+      const Position currentCell,
+      const std::vector<Position>& referencedCells) const = 0;
 };
 
-class Cell : public CellInterface, public GraphInterface {
+class Cell : public CellInterface {
  public:
   Cell(Sheet& sheet);
   ~Cell();
@@ -40,24 +51,9 @@ class Cell : public CellInterface, public GraphInterface {
   Value GetValue() const override;
   std::string GetText() const override;
 
-  std::vector<Position> GetReferencedCells() const override;
-  // Используется Position, т.к. сказано, что нельзя менять CellInterface,
-  // поэтому нет смысла использовать указатели на ячейки
-  std::vector<Position> GetDependentCells() const override;
-  // Если ячейка не создана, создаем EmptyImpl и в нее добавляем dependent cell
-  void AddDependentCell(const Position pos) override;
-  void DeleteDependentCell(const Position pos) override;
-
   void InvalidateCache() const;
 
  private:
-  // https://neerc.ifmo.ru/wiki/index.php?title=%D0%98%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5_%D0%BE%D0%B1%D1%85%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%B3%D0%BB%D1%83%D0%B1%D0%B8%D0%BD%D1%83_%D0%B4%D0%BB%D1%8F_%D0%BF%D0%BE%D0%B8%D1%81%D0%BA%D0%B0_%D1%86%D0%B8%D0%BA%D0%BB%D0%B0
-  // https://neerc.ifmo.ru/wiki/index.php?title=%D0%9E%D0%B1%D1%85%D0%BE%D0%B4_%D0%B2_%D0%B3%D0%BB%D1%83%D0%B1%D0%B8%D0%BD%D1%83,_%D1%86%D0%B2%D0%B5%D1%82%D0%B0_%D0%B2%D0%B5%D1%80%D1%88%D0%B8%D0%BD
-  bool HasCircularDependency(
-      std::unordered_set<CellInterface*>& visitedCells,
-      const Position currentCell,
-      const std::vector<Position>& referencedCells) const;
-
  private:
   class Impl;
   class EmptyImpl;
@@ -81,7 +77,7 @@ class Cell::Impl : public GraphInterface {
 
 class Cell::FormulaImpl : public Impl {
  public:
-  FormulaImpl(const std::string& text);
+  FormulaImpl(const SheetInterface& sheet, const std::string& text);
 
   void Set(const std::string& text);
 
@@ -104,6 +100,7 @@ class Cell::FormulaImpl : public Impl {
   void InvalidateCache() const override;
 
  private:
+  SheetInterface& sheet_;
   std::unique_ptr<FormulaInterface> formula_;
   std::unordered_set<Position> dependentCells_;
   mutable std::optional<CellInterface::Value> cache_;
