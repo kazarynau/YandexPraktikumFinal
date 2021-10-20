05ef40e8d03332dd9b2c26e8c5d83f06d426119b
Siarhei Kazarynau <skazarynau@charlesworthresearch.com>
2021-10-20 22:42:33 +0300

    Remove recursion from HasCircularDependency

--- headers/cell.h
+++ headers/cell.h
@@ -21,10 +21,7 @@ class GraphInterface {
 
   // https://neerc.ifmo.ru/wiki/index.php?title=%D0%98%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5_%D0%BE%D0%B1%D1%85%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%B3%D0%BB%D1%83%D0%B1%D0%B8%D0%BD%D1%83_%D0%B4%D0%BB%D1%8F_%D0%BF%D0%BE%D0%B8%D1%81%D0%BA%D0%B0_%D1%86%D0%B8%D0%BA%D0%BB%D0%B0
   // https://neerc.ifmo.ru/wiki/index.php?title=%D0%9E%D0%B1%D1%85%D0%BE%D0%B4_%D0%B2_%D0%B3%D0%BB%D1%83%D0%B1%D0%B8%D0%BD%D1%83,_%D1%86%D0%B2%D0%B5%D1%82%D0%B0_%D0%B2%D0%B5%D1%80%D1%88%D0%B8%D0%BD
-  virtual bool HasCircularDependency(
-      std::unordered_set<CellInterface*>& visitedCells,
-      const Position currentCell,
-      const std::vector<Position>& referencedCells) const = 0;
+  virtual bool HasCircularDependency(const Position rootPos) const = 0;
 };
 
 class Cell : public CellInterface {
